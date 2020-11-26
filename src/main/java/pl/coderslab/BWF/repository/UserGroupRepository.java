55549711b2317f8e7be5ed3728976f4dddc36429
SzymonTomalik <stomalik@gmail.com>
2020-11-26 21:55:44 +0100

    rename group class and some fixes

--- src/main/java/pl/coderslab/BWF/repository/UserGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab.BWF.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.BWF.entity.UserGroup;
-
-public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
-}
