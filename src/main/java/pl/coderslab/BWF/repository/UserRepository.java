3d8d0dd119ce0c1f638e5b7394d4c399fc893a43
SzymonTomalik <stomalik@gmail.com>
2020-11-26 21:59:38 +0100

    native query findUsersByBetGroup

--- src/main/java/pl/coderslab/BWF/repository/UserRepository.java
+++ src/main/java/pl/coderslab/BWF/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package pl.coderslab.BWF.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.BWF.entity.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM users JOIN user_group_accounts uga on users.id = uga.user_id WHERE user_group_id =?;")
+    public List<User> findUsersByBetGroup(Long id);
 }
