de92022c9650c894688f62c608d0acdaf7cd0f60
SzymonTomalik <stomalik@gmail.com>
2021-03-28 19:09:58 +0200

    small fixes

--- src/main/java/pl/coderslab/BWF/repository/UserRepository.java
+++ src/main/java/pl/coderslab/BWF/repository/UserRepository.java
@@ -10,13 +10,13 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(nativeQuery = true, value = "SELECT * FROM users JOIN user_group_accounts uga on users.id = uga.user_id WHERE bet_group_id =?;")
-    public List<User> findUsersByBetGroup(Long id);
+    List<User> findUsersByBetGroup(Long id);
 
-    public User findUserById(Long id);
+    User findUserById(Long id);
 
-    public User findUserByLogin(String login);
+    User findUserByLogin(String login);
 
     @Query(nativeQuery = true, value ="SELECT * FROM users JOIN user_group_accounts uga on users.id = uga.user_id WHERE bet_group_id =? ORDER BY uga.bets_points DESC;")
-    public List<User> findUsersByBetGroupsOrderByBetPoints(Long id);
+    List<User> findUsersByBetGroupsOrderByBetPoints(Long id);
 
 }
