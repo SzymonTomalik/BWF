de15b8cca629cbe958927481b72d3a632eafea4f
SzymonTomalik <stomalik@gmail.com>
2021-03-28 22:12:34 +0200

    ApiTokens

---
+++ src/main/java/pl/coderslab/BWF/webClient/matches/ApiTokens.java
@@ -0,0 +1,7 @@
+package pl.coderslab.BWF.webClient.matches;
+
+public class ApiTokens {
+    protected static final String URL_CL_MATCHES = "http://api.football-data.org/v2/competitions/CL/matches";
+    protected static final String HEADER = "X-Auth-Token";
+    protected static final String API_TOKEN = "a882976391e54e198bdb9ace56e68e95";
+}
